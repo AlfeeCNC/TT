12d74faa9f9ce74606b9859d8da3ec9e413e9b18
@AlfeeCNC <j0970655711@gmail.com>
2022-08-23 16:28:51 +0800

    update：頁面中英切換功能

--- TriggerToken/urls.py
+++ TriggerToken/urls.py
@@ -16,16 +16,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import JavaScriptCatalog
+
+js_info_dict = {
+    'domain': 'djangojs',
+    'packages': ('MemberSystem', 'Club'),  # my app name
+}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n'))
-    # path('clubs/', include('Club.urls')),
-    # path('members/', include('MemberSystem.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
     # path(<your_path>, include(('<your_app>.urls', '<your_app>')),
     path('clubs/', include('Club.urls')),
     path('members/', include('MemberSystem.urls')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 )
--- static/js/websiteFunctions/chooseRiskPool.js
+++ static/js/websiteFunctions/chooseRiskPool.js
@@ -1,4 +1,4 @@
-var joinType = { "個人": ['請選擇進行方式', '分擔'], "團體": ['請選擇進行方式', '分擔', '互助'] };
+var joinType = { "個人": [gettext("請選擇進行方式"), gettext("分擔")], "團體": [gettext("請選擇進行方式"), gettext("分擔"), gettext("互助")] };
 var joinType1 = document.getElementById("join_type1");
 var joinType2 = document.getElementById("join_type2");
 //設定監聽器
@@ -26,14 +26,14 @@ joinType2.addEventListener('change', function () {
     var btns = document.getElementById('btns')
 
     if (joinType1.value == '個人') {
-        btns.innerHTML = '<a class="mt-4 btn btn-success w-100" href="/clubs/startPlan/individualPlan">創建個人分擔計畫</a>'
+        btns.innerHTML = '<a class="mt-4 btn btn-success w-100" href="/clubs/startPlan/individualPlan">'+gettext("創建個人分擔計畫")+'</a>'
     }
     else if (joinType1.value == '團體') {
-        if (joinType2.value == '分擔') {
-            btns.innerHTML = '<a class="mt-4 btn btn-success w-100" href="/clubs/startPlan/createShareClub"}>創建風險分擔群組</a> <a class="mt-1 btn btn-warning w-100" href={% url "startPlanForm" %}>加入現有群組</a>'
+        if (joinType2.value == '分擔' || joinType2.value == 'Risk share') {
+            btns.innerHTML = '<a class="mt-4 btn btn-success w-100" href="/clubs/startPlan/createShareClub"}>'+gettext("創建風險分擔群組")+'</a> <a class="mt-1 btn btn-warning w-100" href={% url "startPlanForm" %}>'+gettext("加入現有群組")+'</a>'
         }
-        else if (joinType2.value == '互助') {
-            btns.innerHTML = '<a class="mt-4 btn btn-success w-100" href="/clubs/startPlan/createMutualHelpClub" %}>創建風險互助群組</a> <a class="mt-1 btn btn-warning w-100" href={% url "startPlanForm" %}>加入現有群組</a>'
+        else if (joinType2.value == '互助' || joinType2.value == 'Risk mutual') {
+            btns.innerHTML = '<a class="mt-4 btn btn-success w-100" href="/clubs/startPlan/createMutualHelpClub" %}>'+gettext("創建風險互助群組")+'</a> <a class="mt-1 btn btn-warning w-100" href={% url "startPlanForm" %}>'+gettext("加入現有群組")+'</a>'
         }
     }
 })
