fc07803a88a1bd8af3f6f499ad2c3fa59ce2149c
@AlfeeCNC <j0970655711@gmail.com>
2022-08-30 23:51:55 +0800

    add：註冊、登入成功 js 跳轉

---
+++ static/js/websiteFunctions/registerSucceed.js
@@ -0,0 +1,14 @@
+onload = function(){
+    setInterval(go, 1000);
+}
+
+var time = 2;
+function go() {
+    if (time >= 0){
+        document.getElementById("countDown").innerText = time ;
+    }
+    else{
+        location.href="../login";
+    }
+    time--;
+}
